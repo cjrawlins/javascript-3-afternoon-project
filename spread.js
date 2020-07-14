90c04a9173b2eed19d28bcf10e4d6ad8d3f4f9c2
cjrawlins <caseyrawlins@gmail.com>
2020-07-13 19:17:39 -0600

    spread done

--- spread.js
+++ spread.js
@@ -18,7 +18,7 @@ let product = {
 }
 
 // code here
-
+let productCopy = {...product};
 
 
 
@@ -29,7 +29,7 @@ let product = {
 let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
 // code here
-
+let colorsCopy = {...colors};
 
 
 
@@ -44,7 +44,9 @@ function addNums(num1, num2, num3) {
 }
 
 // code here
+let numbersCopy = [...numbers];
 
+let result = addNums(numbersCopy[0],numbersCopy[1],numbersCopy[2]);
 
 ////////// PROBLEM 4 //////////
 
@@ -54,3 +56,5 @@ let arr1 = ['dog', 'chameleon', 'monkey'];
 let arr2 = ['bear', 'tiger', 'liger'];
 
 // code here
+
+let combinedArrays = [...arr1, ...arr2];
